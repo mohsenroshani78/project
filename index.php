b92a2335ea6ee576909d62cc88d89c494fcd6549
mohsen <m.barti09821@gmail.com>
2023-07-26 18:32:00 +0330

    add 3 files to project

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+
+<body>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<button style="background-color: #00BFFF; color: white; font-family: bnazanin; font-size: 16px; padding: 10px 20px; border-radius: 5px;">کلیک کنید</button>
+
+<h3>hello again</h3>
+
+
+
+<header>
+  <nav>
+    <a href="about.html">about</a>
+    <a href="contact.html">contact</a>
+  </nav>
+</header>
+
+</body>
+</html>
